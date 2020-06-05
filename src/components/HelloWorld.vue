d10a0e1d6087b9a93c70a9b69a32872d06614686
18101291613@163.com <18101291613@163.com>
2020-06-05 09:57:08 +0800

    表格可编辑

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,63 +1,42 @@
 <template>
   <div class="hello">
-    <el-table ref="singleTable" :data="tableData" highlight-current-row @current-change="handleCurrentChange" style="width: 100%">
-    <el-table-column type="index" width="50"></el-table-column>
-    <el-table-column label="" width="65">
+    <el-table ref="singleTable" :data="tableData" highlight-current-row @current-change="handleCurrentChange" @cell-dblclick="tableDbEdit" style="width: 100%">
+    <el-table-column label="编号" type="index" width="50"></el-table-column>
+    <el-table-column label="单选" width="65">
       <template slot-scope="scope">
         <div>
-          <el-radio :label="scope.row.date" v-model="templateRadio" @change.native="getTemplateRow(scope.$index,scope.row)">
-             &nbsp;
-          </el-radio>
+          <el-radio :label="scope.row.date" v-model="templateRadio" @change.native="getTemplateRow(scope.$index,scope.row)">&nbsp;</el-radio>
         </div>
       </template>
     </el-table-column>
     <el-table-column property="date" label="日期"></el-table-column>
-    <el-table-column label="姓名" width="180">
-      <template slot-scope="scope">
-        <div>
-          <input type="text" v-model="scope.row.name" :ref="`myInput${scope.$index}`">
-          <div @dblclick="edit(scope.$index)" style="width:130px;border:none;cursor:pointer;">{{scope.row.name}}</div>
-        </div>
-      </template>
-    </el-table-column>
-    <el-table-column property="address" label="地址">
-      <template slot-scope="scope">
-        <div @dblclick="edit">
-          <input type="text" :disabled="editName" :class="{'pointer':editName}" v-model="scope.row.address" @blur="handleEdit(scope.$index, scope.row)" placeholder="请输入内容" >
-        </div>
-      </template>
-    </el-table-column>
+    <el-table-column property="name" label="姓名"></el-table-column>
+    <el-table-column property="address" label="地址"></el-table-column>
   </el-table>
-  <!-- <div style="margin-top: 20px">
-    <el-button @click="setCurrent(tableData[1])">选中第二行</el-button>
-    <el-button @click="setCurrent()">取消选择</el-button>
-  </div> -->
   </div>
 </template>
-
 <script>
 export default {
   name: 'HelloWorld',
   data () {
     return {
       templateRadio:'2016-05-04',
-      editName:false,
       tableData: [{
           date: '2016-05-02',
           name: '王1',
-          address: '上海市普陀区金沙江路 1518 弄'
+          address: '北京市丰台区1号'
         }, {
           date: '2016-05-04',
           name: '王2',
-          address: '上海市普陀区金沙江路 1517 弄'
+          address: '北京市丰台区2号'
         }, {
           date: '2016-05-01',
           name: '王3',
-          address: '上海市普陀区金沙江路 1519 弄'
+          address: '北京市丰台区3号'
         }, {
           date: '2016-05-03',
           name: '王4',
-          address: '上海市普陀区金沙江路 1516 弄'
+          address: '北京市丰台区4号'
         }],
         currentRow: null
     }
@@ -72,17 +51,22 @@ export default {
       getTemplateRow(index,row){
         console.log(index,row)
       },
-      edit(index){
-        console.log(index,`myInput${index}`)
-        this.editName=false;
-        this.$refs[`myInput${index}`].focus()
-      },
-      handleEdit(index,row){
-        // console.log(index, row);
-        this.editName=true;
-      },
-      changeInput(){
-
+      // 双击单元格可编辑
+      tableDbEdit(row, column, cell, event){
+        if (column.label != "单选") {
+            let texts = event.target.innerText
+            event.target.innerHTML = "";
+            let cellInput = document.createElement("input");
+            cellInput.value = texts;
+            cellInput.setAttribute("type", "text");
+            cellInput.style.width = "80%";
+            cell.appendChild(cellInput);
+            cellInput.focus()
+            cellInput.onblur = function() {
+              cell.removeChild(cellInput);
+              event.target.innerHTML = cellInput.value;
+            };
+        }
       }
     }
 }
@@ -95,11 +79,5 @@ export default {
 }
 .pointer{
   cursor:pointer;
-}
-.focus{
-
-}
-.isborder{
-  border:none;
 }
 </style>
